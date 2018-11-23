a734754ecf2042d7a6dfcbbeae965a99fbe611d3
coudrew <couture.andrew@gmail.com>
2018-11-23 16:18:48 +0000

    refactor

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { qndi } from './middleware/qndi';
+import { qndi, handlers } from './middleware/qndi';
 import rootReducer from './reducers';
 import logo from './logo.svg';
 import './App.css';
 import Main from './containers/Main';
+console.log(handlers);
+const logic = qndi(handlers);
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(qndi), 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(logic), 
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   ) 
 );
 
--- src/middleware/qndi.js
+++ src/middleware/qndi.js
@@ -1,22 +1,53 @@
 import { CAT_BREED_FOREST, SET_CAT_BREED, ADD_ACTION, SET_ACTIONS } from '../actions';
 
-export const qndi = ({ getState, dispatch }) => next => action => {
-    // console.log(action);
-    if (action.type !== SET_ACTIONS){
-        const { actions } = getState().actionLog;
-        console.log(actions);
-        const nextActions = [...actions, action];
-        dispatch({
-            type: SET_ACTIONS,
-            actions: nextActions
-        });
-    }
+// export const qndi = ({ getState, dispatch }) => next => action => {
+//     // console.log(action);
+//     if (action.type !== SET_ACTIONS){
+//         const { actions } = getState().actionLog;
+//         console.log(actions);
+//         const nextActions = [...actions, action];
+//         dispatch({
+//             type: SET_ACTIONS,
+//             actions: nextActions
+//         });
+//     }
     
-    if (action.type === CAT_BREED_FOREST) {
-        return dispatch({
-            type: SET_CAT_BREED,
-            breed: 'Forest'
-        });
+//     if (action.type === CAT_BREED_FOREST) {
+//         return dispatch({
+//             type: SET_CAT_BREED,
+//             breed: 'Forest'
+//         });
+//     }
+//     return next(action);
+// }
+
+
+export const qndi = handlers => {
+    const logic = handlers.reduce((cur, acc) => {
+        acc = {
+            ...acc,
+            cur
+        };
+        return acc;
+    }, {});
+
+    return store => next => async action => {
+        const { type } = action;
+        console.log(logic)
+        if (logic[type]) {
+            return await logic[type](store, action).then(() => next(action)); 
+        }
+        return next(action);
     }
-    return next(action);
 }
+
+export const catBreedForest = async ({ dispatch }) => dispatch({
+    type: SET_CAT_BREED,
+    breed: 'Forest'
+}).then(() => {
+    setTimeout(() => dispatch({ type: SET_CAT_BREED, breed: 'Or whatever' }), 2000);
+});
+
+export const handlers = [
+    { [CAT_BREED_FOREST]: catBreedForest }
+];
